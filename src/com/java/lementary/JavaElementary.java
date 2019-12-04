185beff547b034b9723935dcff6fc83ded88a418
Acer <misha127831@gmail.com>
2019-12-04 19:12:16 +0200

    Calculator made

--- src/com/java/lementary/JavaElementary.java
+++ src/com/java/lementary/JavaElementary.java
@@ -1,12 +1,59 @@
 package com.java.lementary;
+import java.util.Scanner;
 
 public class JavaElementary {
+    private static Scanner read;
     public static void main(String[] args) {
-        int z=5;
-        int d=2;
-        int s=z+d;
-        char var = 75;
-        System.out.println(var);
-        System.out.println(s);
+
+        read = new Scanner(System.in);
+
+        double first;
+        double second;
+        int operator;
+
+        System.out.println("Введите первое число:");
+        first = read.nextDouble();
+        System.out.println("Введите второе число:");
+        second = read.nextDouble();
+        System.out.println();
+        System.out.println("Введите число соответствующее нужному вам жейстивию: 1-Сумма, 2-разница, 3-произведение, 4-деление");
+        operator = read.nextInt();
+
+        if (operator == 1) {
+            System.out.println(sum(first, second));
+        }
+        if (operator == 2) {
+            System.out.println(difference(first, second));
+        }
+        if (operator == 3) {
+            System.out.println(multiplication(first, second));
+        }
+        if (operator == 4) {
+            System.out.println(division(first, second));
+        }
+
     }
+    private static double sum(double firstN, double secondN){
+        double answer;
+        answer = firstN + secondN;
+        return answer;
+    }
+    private static double difference(double firstN, double secondN){
+        double answer;
+        answer = firstN - secondN;
+        return answer;
+    }
+    private static double multiplication(double firstN, double secondN){
+        double answer;
+        answer = firstN * secondN;
+        return answer;
+    }
+    private static double division(double firstN, double secondN){
+        double answer;
+        answer = firstN / secondN;
+        return answer;
+    }
+
+
 }
+
